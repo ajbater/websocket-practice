382d60e95257cffbce963587e2d75a73d4ca47bf
ajbater <amanda22@hotmail.co.uk>
2017-02-02 14:13:54 +0000

    can connect to websockets and post messages to terminal

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+// creates a new instance of socket.io by passing the http object
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+// listens for incoming sockets, logs a message to the console
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
